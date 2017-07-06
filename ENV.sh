5c3fbc3ba85e62969e402e8cf538c39772d9d00f
Gaurav Kumar <gaurav.bison@gmail.com>
2017-07-06 14:09:19 -0400

    Added script to check if mxnet is installed. Copied tool scripts locally

--- ENV.sh
+++ ENV.sh
@@ -1,9 +1,9 @@
 # The tokenizer
-export TOKENIZER=/pylon2/ci560op/fosterg/tools/mosesdecoder/scripts/tokenizer/tokenizer.perl
+export TOKENIZER=tools/tokenizer.perl
 #export TOKENIZER=$MOSES/scripts/tokenizer/tokenizer.perl
 
 # Update this location to match your sockeye install
 export SOCKEYE=
-#export SOCKEYE=/home/ws15gkumar/work/code/sockeye
+#export SOCKEYE=~/Dropbox/Projects/sockeye
 
-export EVAL_SCRIPT=/pylon2/ci560op/fosterg/tools/mosesdecoder/scripts/generic/multi-bleu.perl
+export EVAL_SCRIPT=tools/multi-bleu.perl
